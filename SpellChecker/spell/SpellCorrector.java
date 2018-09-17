965340f313b9af9b48431805e31413c380737208
tannerstucki <35358946+tannerstucki@users.noreply.github.com>
2018-09-17 13:15:31 -0600

    Add files via upload

---
+++ SpellChecker/spell/SpellCorrector.java
@@ -0,0 +1,190 @@
+package spell;
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class SpellCorrector implements ISpellCorrector{
+	//data members
+	private Trie launch = new Trie();
+	Trie matchTester = new Trie();
+	//private Set<String> editSet = new HashSet<String>();
+	private List<String> editList = new ArrayList<>();
+	private List<String> editList2 = new ArrayList<>();
+	boolean Distance2 = false;
+	//constructor
+	public SpellCorrector(){}
+
+	//functions
+		public void useDictionary(String dictionaryFileName) throws IOException{
+			//System.out.println(dictionaryFileName);
+			Scanner scan = new Scanner(new File(dictionaryFileName));
+			while(scan.hasNext()){
+				String file_in = scan.next();
+				if(isAlpha(file_in)){
+					launch.add(file_in.toLowerCase());
+				}
+			}
+			scan.close();
+
+			//all this was to test equals
+			/*Scanner scan2 = new Scanner(new File("tester.txt"));
+			while(scan2.hasNext()){
+				String file_in2 = scan2.next();
+				if(isAlpha(file_in2)){
+					matchTester.add(file_in2.toLowerCase());
+				}
+			}
+			scan2.close();
+			System.out.println(launch.equals(matchTester));*/ //to test equals
+
+			//System.out.print(launch.toString()); // to test toString
+			//System.out.println(launch.find("chicken")); // to test find
+			//suggestSimilarWord("dvk");
+		}
+
+		public boolean isAlpha(String name){
+			return name.matches("[A-Za-z]+");
+		}
+
+	/**
+	 * Suggest a word from the dictionary that most closely matches
+	 * <code>inputWord</code>
+	 * @param inputWord
+	 * @return The suggestion or null if there is no similar word in the dictionary
+	 */
+	public String suggestSimilarWord(String inputWord){
+		editList.clear();
+		//editSet.clear();
+		Distance2 = false;
+		if (launch.find(inputWord) != null) {
+			return inputWord;
+		}
+
+		Deletion(inputWord);
+		Transposition(inputWord);
+		Alteration(inputWord);
+		Insertion(inputWord);
+		Distance2 = true;
+
+		//System.out.println(editList.size());
+		//System.out.println(editList);
+
+		String replaceWord = checkList(editList);
+
+		if (replaceWord == null) {
+			editList2.clear();
+			for (int i = 0; i < editList.size(); i++) {
+				Deletion(editList.get(i));
+				Transposition(editList.get(i));
+				Alteration(editList.get(i));
+				Insertion(editList.get(i));
+			}
+			//System.out.println(editList2.size());
+			//System.out.println(editList2);
+			replaceWord = checkList(editList2);
+		}
+
+		return replaceWord;
+	}
+
+	public String checkList(List<String> editList){
+		int curWordFreq = 0;
+		String replaceWord = null;
+
+		for (int i = 0; i < editList.size(); i++) {
+		if (launch.find(editList.get(i)) != null) {
+			if (curWordFreq < launch.find(editList.get(i)).getValue()) {
+				curWordFreq = launch.find(editList.get(i)).getValue();
+				replaceWord = editList.get(i);
+			}
+			/*System.out.println(launch.find(editList.get(i)));
+			System.out.println(launch.find(editList.get(i)).getValue());
+			System.out.println(editList.get(i));*/
+			}
+		}
+		//System.out.println("\n" + replaceWord);
+		return replaceWord;
+	}
+
+	public void Deletion(String inputWord){
+		char[] input_array = inputWord.toCharArray();
+		StringBuilder tempword = new StringBuilder();
+		for (int i = 0; i < input_array.length; i++){
+			tempword.setLength(0);
+			tempword.append(input_array);
+			tempword.deleteCharAt(i);
+			if (Distance2 == false) {
+				//editSet.add(tempword.toString());
+				editList.add(tempword.toString());
+			}
+			else{
+				editList2.add(tempword.toString());
+			}
+		}
+	}
+
+	public void Transposition(String inputWord){
+		char[] input_array = inputWord.toCharArray();
+		char tempChar;
+		StringBuilder tempword = new StringBuilder();
+		for (int i = 0; i < input_array.length - 1; i++){
+			tempword.setLength(0);
+			tempword.append(input_array);
+			tempChar = input_array[i];
+			tempword.setCharAt(i,input_array[i+1]);
+			tempword.setCharAt(i+1,tempChar);
+			if (Distance2 == false) {
+				//editSet.add(tempword.toString());
+				editList.add(tempword.toString());
+			}
+			else{
+				editList2.add(tempword.toString());
+			}
+		}
+	}
+
+	public void Alteration(String inputWord){
+		char[] input_array = inputWord.toCharArray();
+		int tempChar;
+		StringBuilder tempword = new StringBuilder();
+		for (int i = 0; i < input_array.length; i++){
+			for (int j = 0; j < 26 ; j++) {
+				tempword.setLength(0);
+				tempword.append(input_array);
+				tempChar = 'a' + j;
+				if ((char)tempChar != input_array[i]) {
+					tempword.setCharAt(i,(char)tempChar);
+					if (Distance2 == false) {
+						//editSet.add(tempword.toString());
+						editList.add(tempword.toString());
+					}
+					else{
+						editList2.add(tempword.toString());
+					}
+				}
+			}
+		}
+	}
+
+	public void Insertion(String inputWord){
+		char[] input_array = inputWord.toCharArray();
+		int tempChar;
+		StringBuilder tempword = new StringBuilder();
+		for (int i = 0; i < input_array.length + 1; i++){
+			for (int j = 0; j < 26 ; j++) {
+				tempword.setLength(0);
+				tempword.append(input_array);
+				tempChar = 'a' + j;
+				tempword.insert(i,(char)tempChar);
+				if (Distance2 == false) {
+					//editSet.add(tempword.toString());
+					editList.add(tempword.toString());
+				}
+				else{
+					editList2.add(tempword.toString());
+				}
+			}
+		}
+	}
+}
---
+++ SpellChecker/spell/Trie.java
@@ -0,0 +1,212 @@
+package spell;
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Trie implements ITrie{
+	
+	//data members
+	Node root = new Node();
+	int word_count;
+	int node_count = 1;
+	boolean trieMatch = true;
+	boolean wordFound = true;
+
+
+	//constructor
+	Trie (){
+
+	}
+
+	//functions
+		public void add(String word){
+			char [] word_array = word.toCharArray();
+			Node curNode = root;
+
+			for (int i = 0; i < word_array.length; i++) {
+				int index = word_array[i] - 'a';
+				if (curNode.node_array[index] == null) {
+					curNode.node_array[index] = new Node();
+					node_count++;
+				}
+				curNode = curNode.node_array[index];
+			}
+			if (curNode.frequency == 0) {
+				word_count++;
+			}
+			curNode.frequency++;
+			/*if (word_count < 25) {
+				System.out.println("This word: " + word);
+				System.out.println("Word Count: " + word_count);
+				System.out.println("Node Count: " + node_count);
+				System.out.println("This word's frequency: " + curNode.frequency + "\n");	
+			}*/
+		}
+	
+	/**
+	 * Searches the trie for the specified word
+	 * 
+	 * @param word The word being searched for
+	 * 
+	 * @return A reference to the trie node that represents the word,
+	 * 			or null if the word is not in the trie
+	 */
+	public Node find(String word){ // for testing
+		Node curNode = root;
+		wordFound = true;
+		char [] word_array = word.toCharArray();
+		for (int i = 0; i < word_array.length; i++) {
+			int charInt = word_array[i] - 'a';
+			if (curNode.node_array[charInt] == null) {
+				return null;
+			}
+			curNode = curNode.node_array[charInt];
+		}
+
+		if (curNode.frequency != 0) {
+			return curNode;
+		}
+		return null;
+	}
+
+	//this method works but it needs to be changed to return as above
+	/*public boolean findHelper(String word){ // for testing
+		Node curNode = root;
+		wordFound = true;
+		char [] word_array = word.toCharArray();
+		for (int i = 0; i < word_array.length; i++) {
+			int charInt = word_array[i] - 'a';
+			if (curNode.node_array[charInt] == null) {
+				return false;
+			}
+			curNode = curNode.node_array[charInt];
+		}
+
+		if (curNode.frequency != 0) {
+			return true;
+		}
+		return false;
+	}*/
+	
+	/**
+	 * Returns the number of unique words in the trie
+	 * 
+	 * @return The number of unique words in the trie
+	 */
+	public int getWordCount(){
+		return word_count;
+	}
+	
+	/**
+	 * Returns the number of nodes in the trie
+	 * 
+	 * @return The number of nodes in the trie
+	 */
+	public int getNodeCount(){
+		return node_count;
+	}
+	
+	/**
+	 * The toString specification is as follows:
+	 * For each word, in alphabetical order:
+	 * <word>\n
+	 */
+	@Override
+	public String toString(){
+		StringBuilder curword = new StringBuilder();
+		StringBuilder output = new StringBuilder();
+		toStringHelper(root, curword, output);
+		//System.out.println(output.toString());
+		return output.toString();
+	}
+
+	public void toStringHelper(Node curNode, StringBuilder curword, StringBuilder output){
+		if (curNode.frequency > 0){
+			output.append(curword.toString() + "\n");
+		}
+
+		Node tempNode = new Node();
+		//if (curNode.node_array != null){
+			for (int i = 0; i < curNode.node_array.length; i++) {
+				if (curNode.node_array[i] != null) {
+					int temp = 'a' + i;
+					char tempChar = (char) temp;
+					curword.append(tempChar);
+					tempNode = curNode.node_array[i];
+					toStringHelper(tempNode,curword,output);
+					curword.setLength(curword.length() - 1);
+				}
+		//}
+			}
+	}
+	
+	@Override
+	public int hashCode(){
+		//temp return for testing
+		return 37 * word_count * node_count;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+	//public boolean equals(Trie o){ //for testing this method with a trie object
+		trieMatch = true;
+
+		if (!(o instanceof Trie)) {
+			return false;
+		}
+
+		Trie compare = (Trie) o;
+		Node ogNode = root;
+		Node newNode = compare.root;
+
+		if (word_count != compare.word_count) {
+			return false;
+		}
+		if (node_count != compare.node_count) {
+			return false;
+		}
+
+		return equalsHelper(ogNode, newNode);
+		//return true;
+	}
+
+	public boolean equalsHelper(Node ogCurNode, Node newCurNode){
+		for (int i = 0; i < ogCurNode.node_array.length; i++) {
+			if (ogCurNode.node_array[i] != null) {
+				if (newCurNode.node_array[i] == null) {
+					trieMatch = false;
+				}
+				Node ogTempNode = ogCurNode.node_array[i];
+				Node newTempNode = newCurNode.node_array[i];
+				equalsHelper(ogTempNode, newTempNode);
+			}
+		}
+		return trieMatch;
+		//return true;
+	}
+
+	/**
+	 * Your trie node class should implement the ITrie.INode interface
+	 */
+	public class Node implements ITrie.INode{
+	
+		/**
+		 * Returns the frequency count for the word represented by the node
+		 * 
+		 * @return The frequency count for the word represented by the node
+		 */
+
+		private int frequency;
+		private Node[] node_array = new Node[26];
+
+
+		Node() {
+			frequency = 0;
+		}
+
+		public int getValue(){
+			return frequency;
+		}
+	}
+}
